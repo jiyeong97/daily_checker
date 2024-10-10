751e02b8e844f30f01de0e47d9c6dedde11990ee
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-10-09 19:42:41 -0600

    Daily Date

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  String formattedDate = DateFormat.MMMMd('en_US').format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +21,27 @@ class HomeScreen extends StatelessWidget {
               fontSize: 35,
               fontFamily: GoogleFonts.dongle().fontFamily,
             ),
-          )
+          ),
         ),
       ),
-      body: ListView(
-        scrollDirection: Axis.horizontal,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                formattedDate,
+                style: TextStyle(
+                  fontFamily: GoogleFonts.darumadropOne().fontFamily,
+                  fontSize: 58,
+                  color: const Color(0xffcf6116),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
