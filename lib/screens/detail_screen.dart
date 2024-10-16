a1ff64c6774a608b939b2df0dffcc292998327fa
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-10-15 19:40:07 -0600

    youtube link

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({super.key});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  final videoURL = "https://youtu.be/dJXZRZvqbYg?si=aiu28M5H8rrK9PgE";
+
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    final videoID = YoutubePlayer.convertUrlToId(videoURL);
+
+    _controller = YoutubePlayerController(
+      initialVideoId: videoID!,
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar:AppBar(
+        elevation: 2,
+        backgroundColor: const Color(0xffcf6116),
+        foregroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "The cat is watching you",
+            style: TextStyle(
+              fontSize: 35,
+              fontFamily: GoogleFonts.dongle().fontFamily,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          YoutubePlayer(
+            controller: _controller,
+            showVideoProgressIndicator: true,
+            ),
+        ],
+      ),
+    );
   }
 }
